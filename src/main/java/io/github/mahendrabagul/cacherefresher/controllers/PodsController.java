66ffaa6d515ff58753cf135b9e6299e942fc7b0f
Mahendra Bagul <bagulm123@gmail.com>
2021-09-29 09:46:43 +0530

    exposed new apps

--- src/main/java/io/github/mahendrabagul/cacherefresher/controllers/PodsController.java
+++ src/main/java/io/github/mahendrabagul/cacherefresher/controllers/PodsController.java
@@ -1,13 +1,12 @@
 package io.github.mahendrabagul.cacherefresher.controllers;
 
 import io.github.mahendrabagul.cacherefresher.kubeclient.KubeClient;
-import io.kubernetes.client.openapi.ApiException;
-import io.kubernetes.client.openapi.models.V1Pod;
-import io.kubernetes.client.openapi.models.V1PodList;
-import java.io.IOException;
-import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,24 +17,45 @@ public class PodsController {
   @Autowired
   private KubeClient kubeClient;
 
+  //  @GetMapping
+//  public String getPods() {
+//    String labels = "app=nginx";
+//    V1PodList podsByLabels;
+//    StringBuilder stringBuilder = new StringBuilder();
+//    try {
+//      podsByLabels = this.kubeClient.getPodsByLabels(labels);
+//      for (V1Pod item : podsByLabels.getItems()) {
+//        stringBuilder.append("[");
+//        stringBuilder.append(Objects.requireNonNull(item.getMetadata()).getName());
+//        stringBuilder.append(" | ");
+//        stringBuilder.append(Objects.requireNonNull(item.getStatus()).getPodIP());
+//        stringBuilder.append("]");
+//        stringBuilder.append(",");
+//      }
+//    } catch (IOException | ApiException exception) {
+//      exception.printStackTrace();
+//    }
+//    if (stringBuilder.length() >= 2) {
+//      return stringBuilder.substring(0, stringBuilder.length() - 2);
+//    }
+//    return stringBuilder.toString();
+//  }
   @GetMapping
-  public String getPods() {
-    String labels = "app=nginx";
-    V1PodList podsByLabels;
-    StringBuilder stringBuilder = new StringBuilder();
+  public String getRequest() {
     try {
-      podsByLabels = this.kubeClient.getPodsByLabels(labels);
-      for (V1Pod item : podsByLabels.getItems()) {
-        stringBuilder.append("[");
-        stringBuilder.append(Objects.requireNonNull(item.getMetadata()).getName());
-        stringBuilder.append(" | ");
-        stringBuilder.append(Objects.requireNonNull(item.getStatus()).getPodIP());
-        stringBuilder.append("]");
-        stringBuilder.append(",");
-      }
-    } catch (IOException | ApiException exception) {
-      exception.printStackTrace();
+      Thread.sleep(10000);
+    } catch (InterruptedException interruptedException) {
+      System.out.println(interruptedException);
     }
-    return stringBuilder.substring(0, stringBuilder.length() - 2);
+    return "Mahendra";
+  }
+
+  @PostMapping
+  public ResponseEntity<String> postRequest(@RequestBody String message) {
+    System.out.println("Request received : " + message);
+    if (message.contains("hello")) {
+      return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
   }
 }
